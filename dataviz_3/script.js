48458f429825aee082fc0f8b98b2750459086841
emilioleoni <emilioleoni12@hotmail.com>
2023-03-22 14:12:48 -0300

    Dataviz 4 y 5

--- dataviz_3/script.js
+++ dataviz_3/script.js
@@ -12,11 +12,11 @@ d3.csv('astronautas.csv', d3.autoType).then(data => {
     marks: [
       Plot.barX(data, {
         x: 'eva_mision_hs', fill: 'ocupacion',
-        y: 'ocupacion',
+        y: 'ocupacion'
       }),
     ],
     y: {
-      label: "Ocupación",
+      label: "Ocupación", 
     domain: d3.sort(data, (a, b) => d3.descending(a.eva_mision_hs, b.eva_mision_hs)).map(d => d.ocupacion),
     },
     x: { label:"Horas de mision eva",
--- dataviz_4/script.js
+++ dataviz_4/script.js
@@ -1,27 +1,35 @@
 d3.csv('astronautas.csv', d3.autoType).then(data => {
+  const nth = 3; // Change this to the desired interval
+  const filteredData = data.filter((_, i) => i % nth === 0);
+
+  // Define a color scale
+  const colorScale = d3.scaleSequential()
+    .domain(d3.extent(filteredData, d => d.mision_hs))
+    .interpolator(d3.interpolateReds);
+
   let chart = Plot.plot({
+    y: {
+      label: "Astronautas",
+      type: "band",
+      tickFormat: d => d.toLocaleString("en", {month: "narrow"}),
+    },
     marks: [
-      Plot.barX(data, {
-        x: 'mision_hs',
-        y: 'nombre', fill:'mision_hs',
+      Plot.barX(filteredData, {
+        y: "nombre",
+        x: "mision_hs",
+        sort:{y:"x"},
+        fill: d => colorScale(d.mision_hs) // Use color scale to determine bar color
       }),
+      Plot.ruleX([0])
     ],
-    y: {label:"Nombres",
-      domain: d3.sort(data, (a, b) => d3.descending(a.mision_hs, b.mision_hs)).map(d => d.nombre), 
-    },
-    x: {
-      grid: true, label:"Duración de la mision",
-    },
-    color: {
-      scheme:'reds',
-    },
-    height: 1500,
-    width:800,
-    marginLeft: 190,
     marginBottom: 40,
+    marginLeft: "150",
+    width:"1100",
+    height:600,
     nice:true,
     grid:true,
     line:true,
-  })
+  });
+
   d3.select('#chart').append(() => chart)
-})
+});
