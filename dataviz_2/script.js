6a3fa346952c26b33684b2d06d73be1ccb225edd
ezikaplan <96097788+ezikaplan@users.noreply.github.com>
2023-03-22 16:33:28 -0300

    mundial

--- dataviz_2/script.js
+++ dataviz_2/script.js
@@ -1,29 +1,45 @@
 d3.csv('astronautas.csv', d3.autoType).then(data => {
-  let chart = Plot.plot({
-    marks: [
-      Plot.dot(data, {
-        y: 'mision_hs',
-        x: 'anio_mision',
-        fill: 'nacionalidad',
-        //title:;  AGREGAR TITLE, ME DESCRIBE CADA PUNTO
-      }),
-    ],
-    x:{label:"Año de la mision", labelOffset:30},
-    y:{label:"Duracion de la mision", labelOffset:120},
-    grid: true,
-    line: true,
-    nice: true,
-    marginLeft: 120,
-    marginRight: 60,
-    marginTop: 50,
-    marginBottom: 100,
-    height: 450,
-    width: 800,
-    color: {
-      legend: true,
-    },
-  })
+  // Objeto de colores para cada país
+  const colorScale = d3.scaleOrdinal()
+    .domain(Array.from(new Set(data.map(d => d.nacionalidad))))
+    .range(d3.schemeTableau10); // Usa el esquema de colores Tableau 10
 
-  /* Agrega el chart al DOM */
-  d3.select('#chart').append(() => chart)
-})
+  // Configuración del gráfico
+  const trace = {
+    x: data.map(d => d.anio_mision),
+    y: data.map(d => d.mision_hs),
+    text: data.map(d => `Año de la misión: ${d.anio_mision}<br>Duración de la misión: ${d.mision_hs}<br>Nacionalidad: ${d.nacionalidad}`),
+    mode: 'markers',
+    type: 'scatter',
+    marker: {
+      color: data.map(d => colorScale(d.nacionalidad)), // Asigna colores según el país
+      size: 12, // Aumenta el tamaño de los puntos
+    },
+    // Agrega la leyenda de colores
+    showlegend: true,
+    legend: {
+      traceorder: 'reversed',
+    },
+    transforms: [{
+      type: 'groupby',
+      groups: data.map(d => d.nacionalidad),
+      styles: Array.from(new Set(data.map(d => d.nacionalidad))).map(country => ({
+        target: country,
+        value: {
+          marker: {color: colorScale(country)},
+          name: country,
+        },
+      })),
+    }],
+  };
+
+  const layout = {
+    title: 'Misiones espaciales',
+    xaxis: {title: 'Año de la misión'},
+    yaxis: {title: 'Duración de la misión (horas)'},
+    hovermode: 'closest'
+  };
+
+  // Crear y agregar el gráfico al DOM
+  Plotly.newPlot('chart', [trace], layout);
+});
