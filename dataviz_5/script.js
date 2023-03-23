3a719635105e03516bf84e2d4bdd723017861655
ezikaplan <96097788+ezikaplan@users.noreply.github.com>
2023-03-22 23:31:43 -0300

    quinto grafico

--- dataviz_5/script.js
+++ dataviz_5/script.js
@@ -1,45 +1,56 @@
+function createSliderYears(data) {
+  const years = [...new Set(data.map(d => d.anio_mision))].sort((a, b) => a - b);
+  return years;
+}
+
+function updatePlot(year, data) {
+  const filteredData = data.filter(d => d.anio_mision === year);
+
+  const colorScale = d3.scaleOrdinal()
+    .domain(Array.from(new Set(filteredData.map(d => d.nacionalidad))))
+    .range(d3.schemeTableau10);
+
+  const trace = {
+    x: filteredData.map(d => d.nombre),
+    y: filteredData.map(d => d.mision_hs),
+    text: filteredData.map(d => `Nacionalidad: ${d.nacionalidad}<br>Ocupación: ${d.profesion}<br>Horas de misión: ${d.mision_hs}`),
+    type: 'bar',
+    marker: {
+      color: filteredData.map(d => colorScale(d.nacionalidad)),
+    } //Le dejamos color?
+  
+  };
+
+  const layout = {
+    title: `Misiones Espaciales en ${year}`,
+    xaxis: { title: 'Astronautas' },
+    yaxis: { title: 'Horas de misión' }
+  };
+
+  Plotly.newPlot('chart', [trace], layout);
+}
+
 d3.csv('astronautas.csv', d3.autoType).then(data => {
-  // Sumo el total de cada mision a el astronauta correspondiente
-  const groupedData = d3.rollup(data, group => d3.sum(group, d => d.mision_hs), d => d.nombre);
+  const years = createSliderYears(data);
+  const slider = d3.select('body')
+    .append('input')
+    .attr('type', 'range')
+    .attr('min', years[0])
+    .attr('max', years[years.length - 1])
+    .attr('step', 1)
+    .attr('value', years[0]);
 
-  // Lo convierto en una lista
-  const filteredData = Array.from(groupedData, ([nombre, mision_hs]) => ({ nombre, mision_hs }));
+  const displayYear = d3.select('body')
+    .append('p')
+    .text(`Año seleccionado: ${years[0]}`);
 
-  // Eligo los 25 con mas tiempo de mision total
-  const sortedData = filteredData.sort((a, b) => b.mision_hs - a.mision_hs).slice(0, 25);
-
-  // Elijo el color y como lo sorteo
-  const colorScale = d3.scaleSequential()
-    .domain(d3.extent(sortedData, d => d.mision_hs))
-    .interpolator(d3.interpolateBlues);
-
-  let chart = Plot.plot({
-    y: {
-      label: "Astronautas",
-      type: "band",
-      tickFormat: d => d.toLocaleString("en", {month: "narrow"}),
-    },
-    marks: [
-      Plot.barX(sortedData, {
-        y: "nombre",
-        x: "mision_hs",
-        sort:{y:"x"},
-        fill: d => colorScale(d.mision_hs) // Use color scale to determine bar color
-      }),
-      Plot.ruleX([0])
-    ],
-    x:{label: "Tiempo en mision"},
-    marginLeft: 200,
-    marginRight: 50,
-    marginTop: 50,
-    marginBottom: 100,
-    width:"1100",
-    height:500,
-    nice:true,
-    grid:true,
-    line:true,
+  slider.on('input', function () {
+    const selectedYear = +this.value;
+    displayYear.text(`Año seleccionado: ${selectedYear}`);
+    updatePlot(selectedYear, data);
   });
+  
 
-  d3.select('#chart').append(() => chart)
+  updatePlot(years[0], data);
+  
 });
-
