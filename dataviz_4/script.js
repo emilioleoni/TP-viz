7b1f2b3711ea9a5f28222f4cf766b6eef4f14709
ezikaplan <96097788+ezikaplan@users.noreply.github.com>
2023-04-02 18:20:54 -0300

    Actualizacion

--- dataviz_4/script.js
+++ dataviz_4/script.js
@@ -1,60 +1,59 @@
+function createSliderYears(data) {
+  const years = [...new Set(data.map(d => d.anio_mision))].sort((a, b) => a - b);
+  return years;
+}
+
+function updatePlot(year, data) {
+  const filteredData = data.filter(d => d.anio_mision === year);
+
+  const colorScale = d3.scaleOrdinal()
+    .domain(Array.from(new Set(filteredData.map(d => d.nacionalidad))))
+    .range(d3.schemeTableau10);
+
+  const trace = {
+    x: filteredData.map(d => d.nombre),
+    y: filteredData.map(d => d.mision_hs),
+    text: filteredData.map(d => `Nacionalidad: ${d.nacionalidad}<br>Ocupación: ${d.ocupacion}<br>Horas de misión: ${d.mision_hs}`),
+    type: 'bar',
+    marker: {
+      color: filteredData.map(d => colorScale(d.nacionalidad)),
+    } //Le dejamos color?
+  
+  };
+
+  const layout = {
+    title: `Misiones Espaciales en ${year}`,
+    xaxis: { 
+      title: { text: 'Astronautas', standoff: 10 },
+      automargin: true
+    },
+    yaxis: { title: 'Horas de misión' }
+  };
+
+  Plotly.newPlot('chart', [trace], layout);
+}
+
 d3.csv('astronautas.csv', d3.autoType).then(data => {
-  // Sumo el total de cada mision a el astronauta correspondiente
-  const groupedData = d3.rollup(data, group => d3.sum(group, d => d.mision_hs), d => d.nombre);
+  const years = createSliderYears(data);
+  const slider = d3.select('body')
+    .append('input')
+    .attr('type', 'range')
+    .attr('min', years[0])
+    .attr('max', years[years.length - 1])
+    .attr('step', 1)
+    .attr('value', years[0]);
 
-  // Lo convierto en una lista
-  const filteredData = Array.from(groupedData, ([nombre, mision_hs]) => ({ nombre, mision_hs }));
+  const displayYear = d3.select('body')
+    .append('p')
+    .text(`Año seleccionado: ${years[0]}`);
 
-  // Eligo los 25 con mas tiempo de mision total de forma ascendente
-  const sortedData = filteredData.sort((a, b) => d3.descending(a.mision_hs, b.mision_hs)).slice(0, 25);
-
-  // Suma el tiempo de los primeros 25 astronautas
-  const totalTime = d3.sum(sortedData, d => d.mision_hs);
-
-  // Calcula el tiempo promedio
-  const averageTime = totalTime / 25;
-
-  // Elijo el color y como lo sorteo
-  const colorScale = d3.scaleSequential()
-    .domain(d3.extent(sortedData, d => d.mision_hs))
-    .interpolator(d3.interpolateBlues);
-
-    let chart = Plot.plot({
-      y: {
-        label: "Astronautas",
-        type: "band",
-        tickFormat: d => d.toLocaleString("en", {month: "narrow"}),
-        domain: d3.sort(sortedData, d => -d.mision_hs).map(d => d.nombre),
-      },
-      x: {
-        label: "Tiempo en mision",
-      },
-      marks: [
-        Plot.barX(sortedData, {
-          y: "nombre",
-          x: "mision_hs",
-          fill: d => colorScale(d.mision_hs)
-        }),
-        Plot.ruleX([0]),
-      ],
-      marginLeft: 200,
-      marginRight: 50,
-      marginTop: 50,
-      marginBottom: 100,
-      width: 1100,
-      height: 600,
-      nice: true,
-      grid: true,
-      line: true,
-    });
-  
-    d3.select('#chart')
-      .append(() => chart);
-  
-    d3.select('#chart')
-      .append('text')
-      .attr('x', 600)
-      .attr('y', 640)
-      .attr('text-anchor', 'middle')
-      .text(`Tiempo promedio de mision de los 25 astronautas: ${averageTime.toFixed(2)} hs`);
+  slider.on('input', function () {
+    const selectedYear = +this.value;
+    displayYear.text(`Año seleccionado: ${selectedYear}`);
+    updatePlot(selectedYear, data);
   });
+  
+
+  updatePlot(years[0], data);
+  
+});
